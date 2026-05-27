aa7c1eb6ed6f83f454b92602492bc0ec05596674
rajan-sarkar <rajanpandey.079@kathford.edu.np>
2026-05-27 22:27:09 +0545

    added profile image

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ const App = () => {
 
   // SECURE ARRAY MATCHING YOUR LOCAL IMAGES IN /PUBLIC
   const photoMatrix = [
-    "/img1.jpeg", "/img2.jpeg", "/img3.jpeg", "/img4.jpeg", "/img5.jpeg",
+    "/img12.jpeg","/img1.jpeg", "/img2.jpeg", "/img3.jpeg", "/img4.jpeg", "/img5.jpeg",
     "/img6.jpeg", "/img7.jpeg", "/img8.jpeg", "/img9.jpeg", "/img10.jpeg", "/img11.jpeg"
   ];
 
